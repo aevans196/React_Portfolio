0b44ab86a1fbe9b5d00f1f223f187963a71195a1
AAE <alysae2017@gmail.com>
2024-03-21 21:01:56 -0400

    updated about me content page

---
+++ src/components/contentPages/AboutMe.jsx
@@ -0,0 +1,20 @@
+function AboutMe() {
+
+    return (
+        <section class="flexRow">
+        <h1 id="about-me" class="flexSmallItem">About Me</h1>
+            <section class="flexBigItem">
+                <section class="flexAboutMe">
+                    <p>I am a dedicated student currently enrolled in a coding bootcamp, embarking on an exciting journey to merge my healthcare background 
+                with the world of information technology. With a solid foundation in healthcare, I bring a unique perspective to the realm of coding 
+                and software development. My passion for problem-solving and a commitment to improving patient care have driven me to explore this 
+                dynamic field. I am enthusiastic about utilizing my newfound coding skills to create innovative solutions that enhance the healthcare 
+                industry. As I embrace this transition, I am eager to leverage my diverse experiences and skill set to make a meaningful impact at 
+                the intersection of healthcare and technology.</p>
+                </section>
+            </section>
+        </section>
+    );
+}
+
+export default AboutMe;
