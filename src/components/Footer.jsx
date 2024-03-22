d8660425b9abdfc7d0435da90a234c80c41168ec
AAE <alysae2017@gmail.com>
2024-03-22 16:40:11 -0400

    updated routes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function footer() {
             <section class="center">
                 <ul>
                     <li><a href="https://github.com/aevans196" target="_blank">Github</a></li>
-                    <li><a href="https://www.linkedin.com/in/alysaaevans" target="_blank"></a></li>
+                    <li><a href="https://www.linkedin.com/in/alysaaevans" target="_blank">LinkedIn</a></li>
                 </ul>
             </section>
         </footer>
