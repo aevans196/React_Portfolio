1aa6447363f02e7dcaa51a21e6c0acfc00c53815
AAE <alysae2017@gmail.com>
2024-03-22 16:21:21 -0400

    fixed bug and added public folder

--- src/components/contentPages/Project.jsx
+++ src/components/contentPages/Project.jsx
@@ -1,7 +1,7 @@
 function Project({ project }) {
     return (
         <section className="projectFlexItem CreamColor" style={{ backgroundColor: '#fdebd3', width: '1000px', height: '450px', backgroundImage: `url(${project.image})` }} title={project.title}>
-            <a href={project.liveLink} target="_blank">#fdebd3
+            <a href={project.liveLink} target="_blank">
                 <figure className="projectCardHeader" style={{ backgroundColor: '#023e8aff', color: 'pinkSalmon' }}>
                     <h2>{project.title}</h2>
                     <p>{project.description}</p>
