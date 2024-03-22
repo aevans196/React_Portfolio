8ed41b6945910b926ce7b50770506e338c76954e
AAE <alysae2017@gmail.com>
2024-03-22 15:43:07 -0400

    added public folder and updated files

--- src/components/contentPages/Project.jsx
+++ src/components/contentPages/Project.jsx
@@ -1,6 +1,6 @@
 function Project({ project }) {
     return (
-        <section className="projectFlexItem blueHover" style={{ backgroundColor: '#fdebd3', width: '1000px', height: '450px', backgroundImage: `url(${project.image})` }} title={project.title}>
+        <section className="projectFlexItem CreamColor" style={{ backgroundColor: '#fdebd3', width: '1000px', height: '450px', backgroundImage: `url(${project.image})` }} title={project.title}>
             <a href={project.liveLink} target="_blank">#fdebd3
                 <figure className="projectCardHeader" style={{ backgroundColor: '#023e8aff', color: 'pinkSalmon' }}>
                     <h2>{project.title}</h2>
